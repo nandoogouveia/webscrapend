3f2b98071d200e95e87fe1c272299f1090fce8cf
Fernando Gouveia <gouveiaramos.me@gmail.com>
2019-05-25 22:15:35 +0000

    função da aplicação scraper3

--- scrape3.js
+++ scrape3.js
@@ -6,4 +6,21 @@ const express = require('express');
 const app = express();
 
 
+app.get('/scrape3', (req, res) => {
+    request('https://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth', (error, response, html) => {
+        if (!error && response.statusCode == 200){
+            
+            
+            
+            
+       
+        }
+    });
+   
+});
+
+
+
+
+
 

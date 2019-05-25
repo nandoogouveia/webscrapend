50d78eff669f08e7b7c8a2a5adeb9850e0446b02
Fernando Gouveia <gouveiaramos.me@gmail.com>
2019-05-25 21:50:21 +0000

    função da aplicação scraper2

--- scrape2.js
+++ scrape2.js
@@ -6,3 +6,28 @@ const express = require('express');  //cria um aplicativo/app express, onde a fu
 const app = express();               //é uma função de nível superior exportada pelo módulo.
 
 
+app.get('/scrape2', (req, res) => {                                         //O objeto do aplicativo denota convencionalmente o aplicativo Express. 
+    request('https://www.kabum.com.br/', (error, response, html) => {       //Crie-o chamando a função express() de nível superior exportada pelo módulo Express:
+        if (!error && response.statusCode == 200){
+            
+            const $ = cheerio.load(html);           //carrega a página HTML
+            
+            
+            //res.send($('.drop').text());          
+            //console.log($('.drop').text());       
+            
+            res.send($('.box_page').text())         //exibe resposta da requisição no navegador, tela gráfica
+            console.log($('.box_page').text());     //exibe resposta da requisção no console, no bash
+            
+            
+            
+            
+       
+        }
+    });
+   
+});
+
+
+
+

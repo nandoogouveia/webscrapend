3ec51336b0e015d5a2fa50838f17c3fc7141f970
Fernando Gouveia <gouveiaramos.me@gmail.com>
2019-05-25 21:44:28 +0000

    Cria um aplicativo/app express, onde a função express() é de nível superior

--- scrape2.js
+++ scrape2.js
@@ -1,2 +1,8 @@
 const request = require ('request');  //váriavel request fazendo o require do módulo "request" 
 const cheerio = require ('cheerio');  //forma de como chamamos as bibliotecas no Node 
+
+
+const express = require('express');  //cria um aplicativo/app express, onde a função express() 
+const app = express();               //é uma função de nível superior exportada pelo módulo.
+
+

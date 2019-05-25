3640bb34917f7b369bce70dc75d82e04d8e349ec
Fernando Gouveia <gouveiaramos.me@gmail.com>
2019-05-25 21:19:41 +0000

    Cria uma aplicação express

--- scrape.js
+++ scrape.js
@@ -1,2 +1,8 @@
 const request = require ('request');
 const cheerio = require ('cheerio');
+
+
+const express = require('express'); //cria um aplicativo/app express, onde a função express() 
+const app = express();              //é uma função de nível superior exportada pelo módulo.
+
+

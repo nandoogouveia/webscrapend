8e22624ede68759ed176996e5cc85aa616838585
Fernando Gouveia <gouveiaramos.me@gmail.com>
2019-05-25 21:40:52 +0000

    Váriavel request fazendo o require no módulo

---
+++ scrape2.js
@@ -0,0 +1,2 @@
+const request = require ('request');  //váriavel request fazendo o require do módulo "request" 
+const cheerio = require ('cheerio');  //forma de como chamamos as bibliotecas no Node 

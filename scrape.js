06b6c136b89f9c6e9160855a9f034a06b7df1def
Fernando Gouveia <gouveiaramos.me@gmail.com>
2019-05-25 21:25:13 +0000

    função da aplicação scraper

--- scrape.js
+++ scrape.js
@@ -6,3 +6,21 @@ const express = require('express'); //cria um aplicativo/app express, onde a fun
 const app = express();              //é uma função de nível superior exportada pelo módulo.
 
 
+app.get('/scrape', (req, res) => {
+    request('https://fatecrl.edu.br/', (error, response, html) => {
+        if (!error && response.statusCode == 200){
+            //console.log (html);
+            const $ = cheerio.load(html);
+            
+            //const a = ('.home-description');
+            
+            //    console.log(siteHeading.html());
+            //    console.log(siteHeading.text());
+            // const output = a.find('p').text;
+            res.send($('.home-description').text());
+            console.log($('.home-description').text());
+       
+        }
+    });
+    // res.send('eae');
+});

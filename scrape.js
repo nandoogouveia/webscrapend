6a954e094a6e53298ea8adc532e9597c2f082c69
Fernando Gouveia <gouveiaramos.me@gmail.com>
2019-05-25 21:11:35 +0000

    a partir daqui expressjs

--- scrape.js
+++
@@ -1,9 +0,0 @@
-const request = require ('request')
-const cheerio = require ('cheerio')
-
-request ('https://fatecrl.edu.br/', (error, response, html) => {
-    if (!error && response.statusCode == 200){
-        console.log (html);
-    }
-    
-    });
